c00e7e5b9e0702e6c12a4299920de991c1281b75
smartwalle <smartwalle@gmail.com>
2020-04-01 17:04:41 +0800

    更新测试

--- validator_test.go
+++ validator_test.go
@@ -1,13 +1,12 @@
 package validator
 
 import (
-	"testing"
+	"errors"
 	"fmt"
-	"github.com/smartwalle/errors"
-	"github.com/smartwalle/time4go"
+	"testing"
+	"time"
 )
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
 type Human struct {
 	Name string
 	Age  int
@@ -15,62 +14,59 @@ type Human struct {
 
 func (this *Human) NameValidator(n string) error {
 	if n == "" {
-		return errors.New("1001", "请输入名字")
+		return errors.New("请输入名字")
 	}
 	return nil
 }
 
 func (this Human) AgeValidator(a int) error {
 	if a <= 0 {
-		return errors.New("1002", "请输入年龄")
+		return errors.New("请输入年龄")
 	}
 
 	if a > 100 {
-		return errors.New("1003", "你也太长命了吧")
+		return errors.New("你也太长命了吧")
 	}
 	return nil
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-func TestValidator(t *testing.T) {
-	var h *Human = &Human{}
+func TestValidate(t *testing.T) {
+	var h = &Human{}
 	var r = Validate(&h)
-	if !r.OK() {
+	if !r.Passed() {
 		fmt.Println(r.ErrorList())
 	}
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 type Student struct {
-	Human
+	Human  *Human
 	Number int
-	Time *time4go.Time
+	Time   *time.Time
 }
 
 func (this Student) NameValidator(n string) error {
-	return nil
+	return errors.New("不管输入什么，都不会通过的")
 }
 
 func (this Student) NumberValidator(n int) error {
 	if n <= 0 {
-		return errors.New("1004", "应该有一个学号")
+		return errors.New("应该有一个学号")
 	}
 	return nil
 }
 
-func (this Student) TimeValidator(p *time4go.Time) error {
+func (this Student) TimeValidator(p *time.Time) error {
 	if p == nil {
-		return errors.New("1005", "oh no")
+		return errors.New("oh no")
 	}
 	return nil
 }
 
-func TestLazyValidator(t *testing.T) {
-	var s Student
+func TestLazyValidate(t *testing.T) {
+	var s = &Student{Human: &Human{}}
 
-	var r = Validate(&s)
-	if !r.OK() {
+	var r = LazyValidate(&s)
+	if !r.Passed() {
 		fmt.Println(r.ErrorList())
 	}
 }
